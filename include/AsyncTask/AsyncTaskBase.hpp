55c70a17ac6cb237f03d7b796d3cb0707f169938
folosuru <folosuru@gmail.com>
2023-10-12 18:49:02 +0900

    text

--- include/AsyncTask/AsyncTaskBase.hpp
+++ include/AsyncTask/AsyncTaskBase.hpp
@@ -3,10 +3,23 @@
 #define TES_THREADING_HEADER_THREADING_TASKBASE_HPP_
 #include "dll_declspec.hpp"
 namespace AsyncTask {
+/**
+ * AsyncTaskBase
+ *
+ * to use AsyncTask, extend this class...
+ */
 class ASYNC_TASK_DLL AsyncTaskBase {
 public:
+    /**
+     * onRun
+     * running in other thread.
+     */
     virtual void onRun() = 0;
 
+    /**
+     * onComplete
+     * running in main thread.
+     */
     virtual void onComplete() = 0;
 };
 }
